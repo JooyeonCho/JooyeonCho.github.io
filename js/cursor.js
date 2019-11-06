0014a1b6b27f486e674c2e16561527a4dfb7132f
조주연 <JuneCho@jojuyeon-ui-MBP.fios-router.home>
2019-11-06 09:21:42 -0500

    1

--- js/cursor.js
+++ js/cursor.js
@@ -1,12 +1,12 @@
 const { useState, useRef, useEffect, useLayoutEffect, createContext } = React;
 
 /**
-                                                                                * Globals
-                                                                                */
+ * Globals
+ */
 
 const CONSTANTS = {
-  assetPath: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/184729" };
-
+  assetPath: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/184729",
+}
 
 const ASSETS = {
   head: `${CONSTANTS.assetPath}/head.svg`,
@@ -15,18 +15,18 @@ const ASSETS = {
   grabbing: `${CONSTANTS.assetPath}/hand.svg`,
   grabbed: `${CONSTANTS.assetPath}/hand-with-cursor.svg`,
   shaka: `${CONSTANTS.assetPath}/hand-surfs-up.svg`
+}
 
-
-  // Preload images
-};Object.keys(ASSETS).forEach(key => {
+// Preload images
+Object.keys(ASSETS).forEach(key => {
   const img = new Image();
   img.src = ASSETS[key];
 });
 
 
 /**
-     * Shared hooks
-     */
+ * Shared hooks
+ */
 
 // Hover state - https://dev.to/spaciecat/hover-states-with-react-hooks-4023
 const useHover = () => {
@@ -37,16 +37,16 @@ const useHover = () => {
   const leave = () => setHovered(false);
 
   useEffect(
-  () => {
-    ref.current.addEventListener("mouseenter", enter);
-    ref.current.addEventListener("mouseleave", leave);
-    return () => {
-      ref.current.removeEventListener("mouseenter", enter);
-      ref.current.removeEventListener("mouseleave", leave);
-    };
-  },
-  [ref]);
-
+    () => {
+      ref.current.addEventListener("mouseenter", enter);
+      ref.current.addEventListener("mouseleave", leave);
+      return () => {
+        ref.current.removeEventListener("mouseenter", enter);
+        ref.current.removeEventListener("mouseleave", leave);
+      };
+    },
+    [ref]
+  );
 
   return [ref, hovered];
 };
@@ -90,8 +90,8 @@ const usePosition = () => {
 
 
 /**
-    * React Components
-    */
+ * React Components
+ */
 
 class App extends React.Component {
   constructor(props) {
@@ -99,82 +99,82 @@ class App extends React.Component {
     this.state = {
       debug: false,
       cursorGrabbed: false,
-      gameOver: false };
-
-
+      gameOver: false,
+    };
+    
     this.handleToggleDebug = this.handleToggleDebug.bind(this);
     this.handleButtonClicked = this.handleButtonClicked.bind(this);
     this.handleCursorGrabbed = this.handleCursorGrabbed.bind(this);
   }
-
+  
   handleToggleDebug() {
     this.setState({
-      debug: !this.state.debug });
-
+      debug: !this.state.debug
+    });
   }
-
+  
   handleCursorGrabbed() {
     this.setState({
-      cursorGrabbed: true });
-
+      cursorGrabbed: true
+    });
     setTimeout(() => {
       this.setState({
-        cursorGrabbed: false });
-
-    }, 2000);
+        cursorGrabbed: false
+      });
+    }, 2000)
   }
 
-
+    
   handleButtonClicked() {
     this.setState({
-      gameOver: true });
-
+      gameOver: true
+    });
     setTimeout(() => {
       this.setState({
-        gameOver: false });
-
-    }, 4000);
+        gameOver: false
+      });
+    }, 4000)
   }
 
   render() {
     const { cursorGrabbed, gameOver, debug } = this.state;
     const screenStyle = cursorGrabbed ? { cursor: "none" } : {};
     const appClass = debug ? "app app--debug" : "app";
-
+    
     return (
-      React.createElement("div", { className: appClass, style: screenStyle },
-      React.createElement("section", { className: "container" },
-      React.createElement("h1", null, "Hello!"),
-      React.createElement("h2", null, "Welcome to the internet."),
-      React.createElement("p", null, "This is a classic website, no traps or weird stuff!"),
-      React.createElement("p", null, "Feel free to browse, relax and, I don't know, click the button down there? Might as well, right?"),
-
-      React.createElement("button", {
-        className: "debug-button",
-        onClick: this.handleToggleDebug }, "Debug")),
-
-
-
-
-      React.createElement("button", {
-        className: "trap-button",
-        onClick: this.handleButtonClicked },
-      gameOver && "Nice one",
-      cursorGrabbed && "Gotcha!",
-      !gameOver && !cursorGrabbed && "Button!"),
-
-
-      React.createElement("div", { className: "grab-zone-wrapper" },
-      React.createElement(GrabZone, {
-        onCursorGrabbed: this.handleCursorGrabbed,
-        cursorGrabbed: cursorGrabbed,
-        gameOver: gameOver }))));
-
-
-
-
-  }}
-
+      <div className={appClass} style={screenStyle}>
+        <section className="container">
+          <h1>Hello!</h1>
+          <h2>Welcome to the internet.</h2>
+          <p>This is a classic website, no traps or weird stuff!</p>
+          <p>Feel free to browse, relax and, I don't know, click the button down there? Might as well, right?</p>
+          
+          <button
+            className="debug-button"
+            onClick={this.handleToggleDebug}>
+              Debug
+          </button>
+        </section>
+        
+        <button
+          className="trap-button"
+          onClick={this.handleButtonClicked}>
+            { gameOver && "Nice one" }
+            { cursorGrabbed && "Gotcha!" }
+            { !gameOver && !cursorGrabbed && "Button!"}
+        </button>
+        
+        <div className="grab-zone-wrapper">
+          <GrabZone
+            onCursorGrabbed={this.handleCursorGrabbed} 
+            cursorGrabbed={cursorGrabbed}
+            gameOver={gameOver}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 // GrabZone (The hover trigger zone)
 const GrabZone = ({ cursorGrabbed, gameOver, onCursorGrabbed }) => {
@@ -193,46 +193,46 @@ const GrabZone = ({ cursorGrabbed, gameOver, onCursorGrabbed }) => {
     state = "grabbed";
   }
   if (gameOver) {
-    state = "shaka";
+    state = "shaka"
   }
-
+  
   // If state is grabbing for a long time, they're being clever!
   useEffect(() => {
-    let timer;
-    if (state === "grabbing") {
-      timer = setTimeout(() => {
-        // Not so clever now, are they?
-        setExtendedArm(true);
-        timer = null;
-      }, 2000);
-    }
-    return () => {
-      setExtendedArm(false);
-      if (timer) {
-        clearTimeout(timer);
+      let timer;
+      if (state === "grabbing") {
+        timer = setTimeout(() => {
+          // Not so clever now, are they?
+          setExtendedArm(true);
+          timer = null;
+        }, 2000);
       }
-    };
-  },
-  [state]);
-
+      return () => {
+        setExtendedArm(false);
+        if (timer) {
+          clearTimeout(timer);
+        }
+      };
+    },
+    [state]
+  );
 
   return (
-    React.createElement("div", { className: "grab-zone", ref: outerRef },
-    React.createElement("div", { className: "grab-zone__debug" },
-    React.createElement("strong", null, "Debug info:"),
-    React.createElement("p", null, "Current state: ", state),
-    React.createElement("p", null, "Extended arm: ", isExtended ? "Yes" : "No")),
-
-    React.createElement("div", { className: "grab-zone__danger", ref: innerRef },
-    React.createElement(Grabber, {
-      state: state,
-      gameOver: gameOver,
-      extended: isExtended,
-      onCursorGrabbed: onCursorGrabbed }))));
-
-
-
-
+    <div className="grab-zone" ref={outerRef}>
+      <div className="grab-zone__debug">
+        <strong>Debug info:</strong>
+        <p>Current state: {state}</p>
+        <p>Extended arm: {isExtended ? "Yes" : "No"}</p>
+      </div>
+      <div className="grab-zone__danger" ref={innerRef}>
+        <Grabber
+          state={state}
+          gameOver={gameOver}
+          extended={isExtended}
+          onCursorGrabbed={onCursorGrabbed}
+        />
+      </div>
+    </div>
+  );
 };
 
 // Grabber (The graphic)
@@ -245,31 +245,31 @@ const Grabber = ({ state, gameOver, extended, onCursorGrabbed }) => {
   const x = position.left + position.width * 0.5;
   const y = position.top + position.height * 0.5;
   const angle = gameOver ? 0 : Math.atan2(mousePos.x - x, -(mousePos.y - y)) * (180 / Math.PI);
-
+  
   // Ensure value is within acceptable range (-75 to 75)
   const rotation = Math.min(Math.max(parseInt(angle), -79), 79);
-
+  
   const grabberClass = `grabber grabber--${state} ${extended && "grabber--extended"}`;
   const wrapperStyle = { transform: `rotate(${rotation}deg)` };
 
   let handImageSrc = ASSETS[state];
 
   return (
-    React.createElement("div", { className: grabberClass },
-    React.createElement("div", { className: "grabber__body" }),
-    React.createElement("img", { className: "grabber__face", src: ASSETS.head }),
-    React.createElement("div", { className: "grabber__arm-wrapper", ref: ref, style: wrapperStyle },
-    React.createElement("div", { className: "grabber__arm" },
-    React.createElement("img", {
-      className: "grabber__hand",
-      src: handImageSrc,
-      onMouseEnter: onCursorGrabbed })))));
-
-
-
-
-
+    <div className={grabberClass}>
+      <div className="grabber__body"></div>
+      <img className="grabber__face" src={ASSETS.head} />
+      <div className="grabber__arm-wrapper" ref={ref} style={wrapperStyle}>
+        <div className="grabber__arm">
+          <img
+            className="grabber__hand"
+            src={handImageSrc}
+            onMouseEnter={onCursorGrabbed}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 // Render app
-ReactDOM.render(React.createElement(App, null), document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
